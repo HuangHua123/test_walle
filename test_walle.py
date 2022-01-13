9ae01c8dba9d5d3c8f369cf50832f42d5150699a
HuangHua123 <951152098@qq.com>
2022-01-13 15:35:54 +0800

    change test_walle as flaskAPI

--- test_walle.py
+++ test_walle.py
@@ -1,8 +1,22 @@
-def add(a,b):
-	return a+b
+from flask import Flask, request, jsonify, make_response
+app = Flask(__name__)
 
-a = 1
-b = 2
-c = add(a,b)
-with open("./result.txt","w",encoding="utf8") as f:
-    f.write("{}+{}={}".format(a,b,c))
+@app.route('/getUser/', methods=['GET'])
+def getUser():
+	name = "张三"
+	sex = "男"
+	age = "24"
+	results = {
+		"名字":name,
+		"性别":sex,
+		"年龄":age
+	}
+	response = make_response(jsonify(results))
+	response.headers['Content-Type'] = 'application/json;charset=UTF-8'
+	response.headers['Server'] = ''
+	return response
+ 
+if __name__ == "__main__":
+    app.config['JSON_AS_ASCII'] = False
+    app.config['JSON_SORT_KEYS'] = False
+    app.run(host='0.0.0.0', port=5014, debug=False, use_reloader=False)
